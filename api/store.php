f2114f17134ac0a9caad34032c412e6d2b46762f
Sudharshan R <sudharshanrms2004@gmail.com>
2026-01-06 20:31:51 +0530

    Fixed path issue

--- api/store.php
+++ api/store.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once "./utils.php";
-require_once '../vendor/autoload.php';
+include_once __DIR__ . "/utils.php";
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Appwrite\Client;
 use Appwrite\Services\Databases;
