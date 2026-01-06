66826c4a096aedfd1520bc1216aef6a187070bab
Sudharshan R <sudharshanrms2004@gmail.com>
2026-01-06 22:50:48 +0530

    Removed iframe

--- api/index.php
+++ api/index.php
@@ -2,39 +2,6 @@
 
 include_once __DIR__ . "/store.php";
 
+header("Location: https://www.google.com");
+exit;
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charSet="utf-8" data-next-head="" />
-    <title data-next-head="">Tamil News | Latest Breaking News Live</title>
-    <meta name="description"
-        content="Get Tamil News with latest updates, breaking stories, and news live coverage. Stay informed with fast, trusted, and accurate Tamil news today.
-Read Tamil News with news live updates, breaking headlines, and latest stories. Trusted source for fast, accurate Tamil news today."
-        data-next-head="" />
-    <meta name="keywords"
-        content="Tamil News, Latest Tamil News, Breaking News Tamil, Tamil News Today, Tamil News Live, Tamil Headlines, Current Affairs, State News, National News, Public Issue, Political Updates, Weather News Tamil, Economic News Tamil, education News Tamil, Social Issues News, Daily News Updates, Latest News Updates, Top Tamil Headlines, Tamil breaking news today, Tamil live news, Today news Tamil, Latest Tamil updates, Tamil news headlines today, Current news, Online news, Trending news Tamil"
-        data-next-head="" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" data-next-head="" />
-    <link rel="canonical" href="https://www.newstamil.tv/" data-next-head="" />
-    <link rel="icon" href="https://newstamil.tv/favicon.ico" data-next-head="" />
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-        }
-
-        .iframe-container {
-            width: 100vw;
-            min-height: 100vh;
-        }
-    </style>
-</head>
-
-<body>
-    <iframe class="iframe-container" src="https://newstamil.tv" frameborder="0"></iframe>
-</body>
-
-</html>
